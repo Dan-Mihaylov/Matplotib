74b6be4f19cc7517471bf678b6440a2e8d2c8c2c
Dan-Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2023-12-17 14:49:04 +0000

    More fin data plotting
    Added some more plotting content

--- 04 Visualize finance data/02_customizations_and_fill.py
+++ 04 Visualize finance data/02_customizations_and_fill.py
@@ -12,25 +12,30 @@ def get_ticker_history(ticker: str, period: str, interval: str) -> yf.Ticker.his
 
 
 mpl.use('Qt5Agg')
-fig, ax1 = plt.subplots(figsize=(9.80, 6.80), layout='constrained')
+
+def plot_ticker(ticker: str):
+
+    fig, ax1 = plt.subplots(figsize=(9.80, 6.80), layout='constrained')
+
+    TICKER = get_ticker_history(ticker, '5d', '1m')
+    timestamps = TICKER.index
+    datetimes = timestamps.to_pydatetime()
+
+    cdf = mpl.dates.ConciseDateFormatter(ax1.xaxis.get_major_locator())
+    ax1.xaxis.set_major_formatter(cdf)
+
+    ax1.set_title(ticker)
+    ax1.set_ylabel('Price')
+    ax1.set_xlabel('Date Time')
+
+    # get the price data
+    prices = [price for price in TICKER.Close]
+
+    ax1.plot(datetimes, prices, '-', label=ticker)
+    ax1.legend()
+    ax1.grid(True, which='both', dashes=(2.5, 2.5), fillstyle='bottom')
+    plt.show()
 
 
-MSFT = 'MSFT'
-msft = get_ticker_history(MSFT, '5d', '15m')
-timestamps = msft.index
-datetimes = timestamps.to_pydatetime()
+plot_ticker('AAPL')
 
-cdf = mpl.dates.ConciseDateFormatter(ax1.xaxis.get_major_locator())
-ax1.xaxis.set_major_formatter(cdf)
-
-ax1.set_title(MSFT)
-ax1.set_ylabel('Price')
-ax1.set_xlabel('Date Time')
-
-# get the price data
-prices = [price for price in msft.Close]
-
-ax1.plot(datetimes, prices, '-', label='MSFT')
-ax1.legend()
-ax1.grid(True, which='both', dashes=(2.5, 2.5), fillstyle='bottom')
-plt.show()
